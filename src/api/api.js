fbabc0479b1778b36fe39e6f668fe5df0dfb2f56
podestal <luisrodriguezpodesta@gmail.com>
2024-01-04 15:37:41 -0500

    signup page

--- src/api/api.js
+++ src/api/api.js
@@ -2,10 +2,13 @@ import axios from "axios"
 
 const BASE_URL = 'http://127.0.0.1:8000/'
 const LOGIN = 'auth/jwt/create/'
+const SIGNUP = 'auth/users/'
 
 const baseAxios = axios.create({
     baseURL: BASE_URL
 })
 
-export const login = async (data) => baseAxios.post(LOGIN, data)
+export const login = async data => baseAxios.post(LOGIN, data)
+
+export const signup = async data => baseAxios.post(SIGNUP, data)
     
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { useMutation } from '@tanstack/react-query'
+import { signup } from '../api/api'
 
 const Signup = () => {
 
@@ -6,38 +8,54 @@ const Signup = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [pwd, setPwd] = useState("")
+    const [err, setErr] = useState("")
+
+    const {mutate} = useMutation({
+        mutationFn: data => signup(data),
+        onSuccess: res => console.log(res),
+        onError: err => console.log(err),
+    })
 
     const handleSubmit = e => {
+        setErr("")
         e.preventDefault()
+        if (password === pwd) {
+            mutate({ email, username, password })
+        }
+        setErr("Passwords must match")
     }
 
   return (
-    <form>
-        <input 
-            type='email'
-            placeholder='Email'
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-        />
-        <input 
-            type='text'
-            placeholder='Username'
-            value={username}
-            onChange={e => setUsername(e.target.value)}
-        />
-        <input 
-            type='password'
-            placeholder='Password'
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-        />
-        <input 
-            type='password'
-            placeholder='Re-Password'
-            value={pwd}
-            onChange={e => setPwd(e.target.value)}
-        />
-    </form>
+    <>
+        <p>{err}</p>
+        <form onSubmit={handleSubmit}>
+            <input 
+                type='email'
+                placeholder='Email'
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+            />
+            <input 
+                type='text'
+                placeholder='Username'
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+            />
+            <input 
+                type='password'
+                placeholder='Password'
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+            />
+            <input 
+                type='password'
+                placeholder='Re-Password'
+                value={pwd}
+                onChange={e => setPwd(e.target.value)}
+            />
+            <button type='submit'>Signup</button>
+        </form>
+    </>
   )
 }
 
