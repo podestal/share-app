da213d9fdf443887d434f964042a2ffcbfadc92a
podestal <luisrodriguezpodesta@gmail.com>
2024-01-10 16:32:28 -0500

    due date

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 const Service = ({ service }) => {
+
+    const navigate = useNavigate()
+
+    const handleJoin = () => {
+        navigate(`/service/${service.id}`)
+    }
+
   return (
     <div>
         <Link to={`/service/${service.id}`}><h3>{service.platform}</h3></Link>
         <p>$.{service.price}.00</p>
-        <button>Unirte</button>
+        <button onClick={handleJoin}>Unirte</button>
     </div>
   )
 }
--- src/components/SubscriptionItem.jsx
+++ src/components/SubscriptionItem.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import moment from 'moment'
 
 const SubscriptionItem = ({ subscription }) => {
+
+    const period = {
+        'T': 30,
+        'S': 60,
+        'N': 90,
+    }
+
+    const dueDate = moment(subscription?.subscribed_at).add(period[subscription?.period], 'days').calendar()
+
   return (
     <div>
         <h2>{subscription.service.platform}</h2>
         <p>Period: {subscription.period}</p>
         <p>User: {subscription.username}</p>
         <p>Password: {subscription.password}</p>
+        <p>Due Date: {dueDate}</p>
     </div>
   )
 }
