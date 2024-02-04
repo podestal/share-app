7524b29fa8af8b57b809058c9a738444e7eb54a5
podestal <luisrodriguezpodesta@gmail.com>
2024-02-04 07:15:20 -0500

    get movies data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import useUser from '../hooks/useUser'
-import useServices from '../hooks/useServices'
 import Services from '../components/Services'
 import MoviesFinder from '../components/MoviesFinder'
 
 const Home = () => {
-
-    const {user} = useUser()
-    const {data: services, isLoading, isError, error} = useServices()
-
-    if (isLoading) return <p>Loading ...</p>
-
-    if (isError) return <p>{error.message}</p>
 
   return (
     <>  
