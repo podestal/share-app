b6060c67e82090b1d558f688d1e86991caae74aa
podestal <luisrodriguezpodesta@gmail.com>
2024-02-04 14:02:56 -0500

    get to the purchase service from logo

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -5,8 +5,13 @@ import { Link } from 'react-router-dom'
 const MoviesPage = () => {
 
     const {movies} = useMovies()
-    const mytr = 'hola'
-    mytr.toLowerCase()
+    const purchaseOptions = {
+        'HBO Max': 8,
+        'Paramount': 4,
+        'Amazon Prime Video': 3,
+        'Disney+': 2,
+        'Netflix': 1
+    }
 
   return (
     <div className='main-body'>
@@ -15,7 +20,7 @@ const MoviesPage = () => {
               <h3>{title.original_title}</h3>
               <p>Streaming at:</p>
               <div className='logos-container'>
-                {title.streaming.map(stream => <div className={`logo ${stream.toLowerCase().replace(' ', '-').replace('+', '')}-logo`}></div> )}
+                {title.streaming.map(stream => <Link to={`/service/${purchaseOptions[stream]}`}><div className={`logo ${stream.toLowerCase().replace(' ', '-').replace('+', '')}-logo`}></div></Link>)}
               </div>
 
             </div>
