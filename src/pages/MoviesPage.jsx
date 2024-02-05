0ef3f12944554a754081a12788d1cbf144ca7dd0
podestal <luisrodriguezpodesta@gmail.com>
2024-02-05 17:24:45 -0500

    getting image poster anyways

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -25,7 +25,8 @@ const MoviesPage = () => {
               </div>
             </div>
             <div className='title-img'>
-              <img src={title.poster} alt={title.original_title} />
+              {/* {title.poster[0] == 'None' && <img src={title.poster[1]} alt={title.original_title} />} */}
+              <img src={title.poster[0] == 'None' ? title.poster[1] : title.poster[0]} alt={title.original_title} />
               {console.log(title.poster)}
             </div>
           </div>
