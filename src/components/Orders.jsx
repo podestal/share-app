f43f97dbca4bbde36b837e2c3a9e92a7fad3eade
podestal <luisrodriguezpodesta@gmail.com>
2024-02-17 07:26:38 -0500

    filtering orders

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Order from './Order'
+import Spinner from './Spinner'
+import { useQuery } from '@tanstack/react-query'
+import { getOrders } from '../api/api'
 
-const Orders = ({ orders }) => {
+
+const Orders = () => {
+
+  const [filter, setFilter] = useState('P')
+  const [access, setAccess] = useState(JSON.parse(localStorage.getItem('access')) || "")
+  const {data: orders, isLoading, isError, error} = useQuery({
+      queryKey: ['orders'],
+      queryFn: () => getOrders({ access })
+  })
+
+  if (isLoading) return <Spinner />
+
+  if (isError) return <p>{error.message}</p>
+
   return (
-    <div className='orders-container'>
-        {orders.map(order => (
-            <Order 
-                key={order.id}
-                order={order}
-            />
+    <>
+      <div className='filter-buttons buttons-container'>
+        <button onClick={() => setFilter('S')} className='btn btn-primary'>Started</button>
+        <button onClick={() => setFilter('P')} className='btn btn-primary'>Processing</button>
+        <button onClick={() => setFilter('C')} className='btn btn-primary'>Completed</button>
+      </div>
+      <div className='orders-container'>
+        {orders.data
+        .filter(order => order.status == filter)
+        .map(order => (
+          <Order 
+              key={order.id}
+              order={order}
+          />
         ))}
-    </div>
+      </div>
+    </>
   )
 }
 
--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -6,21 +6,9 @@ import Spinner from '../components/Spinner'
 
 const OrdersPage = () => {
 
-    const [access, setAccess] = useState(JSON.parse(localStorage.getItem('access')) || "")
-    const {data: orders, isLoading, isError, error} = useQuery({
-        queryKey: ['orders'],
-        queryFn: () => getOrders({ access })
-    })
-
-    if (isLoading) return <Spinner />
-
-    if (isError) return <p>{error.message}</p>
-
   return (
     <div className='main-body'>
-        <Orders 
-          orders={orders.data}
-        />
+        <Orders />
     </div>
   )
 }
